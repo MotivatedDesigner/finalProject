f21854540e1965486bf18db50a6c2fbded022dd3
othmane-boudiab <mrboudiabothmane@gmail.com>
2021-01-18 11:11:11 +0100

    edit function aleatoire v2

--- functions/functions.c
+++ functions/functions.c
@@ -1,12 +1,12 @@
 #include "functions.h"
 
-
 //Get array(int) size
 int getSize(int arr[])
 {
     return *(&arr + 1) - arr;
 }
 
+//Generate random # between start & end
 int aleatoire(int start, int end)
 {
     //Used to reset the seed of the rand generator
@@ -15,4 +15,11 @@ int aleatoire(int start, int end)
     
     //return randum # between start & and
     return  start + (rand() % end-start);
+}
+
+//initialize array[size] with random #
+void initArray(int arr[],int start, int end)
+{
+    for (int i = 0; i < getSize(arr)-1; i++)
+        arr[i] = aleatoire(start,end);
 }
