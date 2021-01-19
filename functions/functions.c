f18a102d87d5536b06eb25024ce0e3aee9d4b92e
MotivatedDesigner <virouss2020@gmail.com>
2021-01-19 11:16:01 +0100

    bubble sort

--- functions/functions.c
+++ functions/functions.c
@@ -26,3 +26,56 @@ void initArray(int arr[], int size, int start, int end)
     //Fill arr with random #
     for (int i = 0; i < size-1; i++) arr[i] = aleatoire(start,end);
 }
+
+//Print array of length n
+void printArray(int arr[], int length)
+{
+    for (int i = 0; i < length; i++) printf("%d",arr[i]);
+}
+
+// Delete repeated elements from array of int
+int* deleteDouble(int arr[], int size, int start)
+{
+    // count will be the size of the returned array
+    int count = size;
+    int tmp;
+    
+    for (int i = 0; i < size; i++) 
+    {
+        
+        // Fill repeated elements with (start-1) as a mark (no element in the initial array will have the value (start-1))
+        if( arr[i] == (start-1) ) continue;
+        
+        tmp = arr[i];
+        
+        for (int j = i+1; j < size; j++) 
+        {
+            
+            // If element found more than once his place will be filled with (start-1) 
+            if( arr[j] == tmp ){ arr[j] = (start-1); count--; }
+        }
+    }
+}
+
+//Helper func to swap to int
+void swap(int* a, int* b)
+{
+    *a^=*b^=*a^=*b;
+}
+
+//Buble Sort
+void bubleSort(int arr[], int size)
+{
+    // Handle if the array is already sorted
+    bool isSorted = True;
+    for(int i = 0; i < 5; i++)
+    {
+        for(int j=0;j<5-i-1;j++)
+            if( arr[j] > arr[j+1] ) 
+            {
+                swap(&arr[j],&arr[j+1]);
+                isSorted = False;
+            }
+        if(isSorted) break;
+    }
+}
